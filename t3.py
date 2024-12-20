463ce5443a28f40306c03c6ca144a3e6526215cd
Fahim Ibn Mizan <147166665+Fahim20010907@users.noreply.github.com>
2024-12-21 00:37:10 +0600

    Add files via upload

---
+++ t3.py
@@ -0,0 +1,35 @@
+# print Fahim's laptop
+print("Fahim's Laptop")
+
+# print fahim er "laptop"
+print('Fahim er "Laptop"')
+
+# ekhon print korte hobe: Fahim's "Laptop"
+# quotation er meaining/action ta change korar jonno er age "\" sign ta use kora hoise
+print("Fahim's \"Laptop\"")
+
+# print fahim 10 times
+a = 'Fahim'
+print(10*a)
+
+# string concatenation
+b = 'Fahim'
+c = ' Ibn Mizan'
+print(b+c+" Al Mahfuz Khan")
+
+
+# new line create korar jonno "\n" command use korte hobe
+print("Fahim Ibn Mizan\nCristiano Ronaldo\nLionel Messi")
+
+#want to print the raw string, for example "Fahim Ibn Mizan\nCristiano Ronaldo\nLionel Messi"
+print(r"Fahim Ibn Mizan\nCristiano Ronaldo\nLionel Messi")
+
+
+
+
+
+
+
+
+
+
